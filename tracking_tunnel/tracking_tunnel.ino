53d57a28a87f6f70b956d683cb82ac5cc7ebdd74
llamington <54869395+llamington@users.noreply.github.com>
2021-04-16 16:25:15 +1200

    fixed distance readings and requires restart if calibration out of range (untested)

--- tracking_tunnel/tracking_tunnel.ino
+++ tracking_tunnel/tracking_tunnel.ino
@@ -2,30 +2,31 @@
 
 #define SERIAL_BAUD_RATE 115200
 
+#define DISP_I2C_ADDR 0x27
+#define DISP_RES_H 20
+#define DISP_RES_V 4
+
 #define DISP_SHOW_BTN_PIN 2
 #define IR_PIN A0
 #define COUNTER_RESET_PIN 3
-#define CAMERA_TRIGGER_PIN LED_BUILTIN // TODO: SET THIS
+#define CAMERA_TRIGGER_PIN 12
 
-#define ADC_MIN 0
-#define ADC_MAX 1023 // 2^10 - 1
+#define ADC_MAX_V 5.0
+#define ADC_MAX 1023.0 // 2^10 - 1 as 10 bit ADC
 
 #define IR_RANGE_MAX 80.0
-#define IR_RANGE_MIN 5.70760233918128
+#define IR_RANGE_MIN 5.7076
 #define IR_OUT_OF_RANGE -1
-#define IR_CURVE_COEFFICIENT 13.5
-#define IR_CURVE_EXP -0.791
+#define IR_CURVE_COEFFICIENT 26.85
+#define IR_CURVE_EXP -1.2642
 
-#define DETECTION_MS 100 // ms required for a detection event
-#define DETECTION_DEV_CM 1 // deviation in width for detection event
+#define DETECTION_DEV_CM 1.0 // deviation in width for detection event
 
 #define CAMERA_TRIGGER_PULSE_MS 30 // pulse that camera requires
-
 #define INITIALISATION_DELAY_MS 1000
-
 #define DISPLAY_TIMEOUT_MS 10000
-
 #define POLLING_DELAY_MS 50
+#define DETECTION_MS 100 // ms required for a detection event
 
 double tunnelWidth;
 float curRange;
@@ -36,7 +37,7 @@ bool dispOn = false;
 uint16_t dispOnTime = 0;
 bool shouldUpdateDisp = false;
 
-LiquidCrystal_I2C lcd(0x27,20,4);
+LiquidCrystal_I2C lcd(DISP_I2C_ADDR, DISP_RES_H, DISP_RES_V);
 
 // runs once
 void setup(void)
@@ -49,20 +50,31 @@ void setup(void)
 
   Serial.begin(SERIAL_BAUD_RATE);
   lcd.init();
+  lcd.backlight();
   lcd.print(F("INITIALISED"));
   Serial.println(F("TRACKING TUNNEL INITIALISED"));
+  
   attachInterrupt(digitalPinToInterrupt(DISP_SHOW_BTN_PIN), handleDispShow, FALLING);
   attachInterrupt(digitalPinToInterrupt(COUNTER_RESET_PIN), handleAnimalCountReset, FALLING);
-  delay(1000);
+  
   tunnelWidth = readRangeCm();
+  if(tunnelWidth == IR_OUT_OF_RANGE) {
+    Serial.println(F("OUT OF RANGE. PLEASE RESET TUNNEL"));
+    lcd.setCursor(0,0);
+    lcd.print(F("OUT OF RANGE."));
+    lcd.setCursor(0, 1);
+    lcd.print(F("RESET TUNNEL"));
+    while(true);
+  }
   lcd.setCursor(0, 0);
-  lcd.print("TUNNEL WIDTH = ");
+  lcd.print(F("TUNNEL WIDTH = "));
   lcd.setCursor(0, 1);
   lcd.print(tunnelWidth);
-  Serial.print("TUNNEL WIDTH = "); //if out of range display error
+  Serial.print(F("TUNNEL WIDTH = "));
   Serial.println(tunnelWidth);
   displayAnimalCount();
   delay(INITIALISATION_DELAY_MS);
+  lcd.noBacklight();
 }
 
 // runs indefinitely
@@ -91,7 +103,6 @@ void loop(void)
       pulseCameraTrigger();
       shouldUpdateDisp = true;
       Serial.println("Animal Detected");
-      delay(100);
     } 
   } else animalInTunnel = false;
   dispOnTime += POLLING_DELAY_MS;
@@ -101,7 +112,7 @@ void loop(void)
 // converts raw ADC output to voltage
 float rawToVoltage(uint16_t raw)
 {
-  return raw * (5.0 / 1023.0);
+  return raw * (ADC_MAX_V / ADC_MAX);
 }
 
 // converts voltage to range in cm
@@ -109,7 +120,6 @@ double voltageToRange(float voltage)
 {
   float range = IR_CURVE_COEFFICIENT * pow(voltage, IR_CURVE_EXP);
   if((range <= IR_RANGE_MAX) && (range >= IR_RANGE_MIN)) {
-    Serial.println(range);
     return range;
   }
   return IR_OUT_OF_RANGE;
@@ -118,11 +128,10 @@ double voltageToRange(float voltage)
 // handler for display show button press
 void handleDispShow(void)
 {
-  // may need debouncing
   dispOn = true;
 }
 
-// handler for 
+// handler for animal count reset
 void handleAnimalCountReset(void)
 {
   // may need debouncing
@@ -138,6 +147,7 @@ float readRangeCm(void)
   return voltageToRange(rangeVolt);
 }
 
+// pulses camera trigger output
 void pulseCameraTrigger(void)
 {
     digitalWrite(CAMERA_TRIGGER_PIN, HIGH);
@@ -145,9 +155,10 @@ void pulseCameraTrigger(void)
     digitalWrite(CAMERA_TRIGGER_PIN, LOW);
 }
 
+// updates LCD with current animal count
 void displayAnimalCount() {
   lcd.clear();
-  lcd.print("Animal Count =");
+  lcd.print(F("Animal Count ="));
   lcd.setCursor(0, 1);
   lcd.print(nPredators);
 }
